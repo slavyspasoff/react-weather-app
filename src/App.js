aca5e383a4fc7ac39c24aa48030e79ee1b966304
slavyspasoff <slavyspasoff@gmail.com>
2021-06-06 15:43:05 +0200

    add better description and small style tweaks

--- src/App.js
+++ src/App.js
@@ -6,35 +6,35 @@ import Homepage from './Homepage';
 import Navbar from './Navbar';
 
 function App() {
-    const [location, setLocation] = useState('');
-    const [data, setData] = useState('');
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get(
-                    `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&appid=7211be16e55aa4ed73d3e5ed5cc194fe`
-                );
-                setData(res.data);
-            } catch (e) {
-                console.log(e);
-            }
-        };
+  const [location, setLocation] = useState('');
+  const [data, setData] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&units=metric&appid=7211be16e55aa4ed73d3e5ed5cc194fe`
+        );
+        setData(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
-        fetchData();
-    }, [location]);
+    fetchData();
+  }, [location]);
 
-    return (
-        <>
-            <Navbar setLocation={setLocation} />
-            <Switch>
-                <Route
-                    exact
-                    path="/"
-                    render={() => <Homepage data={data} location={location} />}
-                />
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      <Navbar setLocation={setLocation} />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <Homepage data={data} location={location} />}
+        />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-
+import { v4 as uuid } from 'uuid';
 import DailyTempTile from './DailyTempTile';
 import LineGraph from './LineGraph';
 
 const useStyles = makeStyles({
-    root: {
-        height: '90vh',
-        width: '100vw',
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#F7F5FB',
-    },
+  root: {
+    height: '90vh',
+    width: '100vw',
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F7F5FB',
+  },
 
-    chartContainer: {
-        height: '35%',
-        width: '55%',
-        flexBasis: '100%',
-    },
+  chartContainer: {
+    height: '35%',
+    width: '55%',
+    flexBasis: '100%',
+  },
 });
 
 export default function Homepage(props) {
-    const { data, location } = props;
-    // console.log(data);
-    const classes = useStyles();
-    const dailyTiles =
-        data &&
-        data.daily.map((e) => {
-            return <DailyTempTile daily={e} />;
-        });
-    console.log(dailyTiles);
-    return (
-        <div className={classes.root}>
-            {data && dailyTiles}
-            <div className={classes.chartContainer}>
-                {data && <LineGraph data={data} location={location} />}
-            </div>
-        </div>
-    );
+  const { data, location } = props;
+  console.log(data);
+  const classes = useStyles();
+  const dailyTiles =
+    data &&
+    data.daily.map((e) => {
+      return <DailyTempTile daily={e} key={uuid()} />;
+    });
+  return (
+    <div className={classes.root}>
+      {data && dailyTiles}
+      <div className={classes.chartContainer}>
+        {data && <LineGraph data={data} location={location} />}
+      </div>
+    </div>
+  );
 }
